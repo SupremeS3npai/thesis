4f06f979c65331e7137b056dcf0d50bd99733e13
SupremeS3npai <100749816+SupremeS3npai@users.noreply.github.com>
2022-06-18 18:13:21 +0800

    Add files via upload

---
+++ frontpage.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="frontpage.css">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+
+<style>
+	body{
+		font-family: 'Raleway', sans-serif;
+	}
+</style>
+
+</head>
+<body>
+ 	
+
+ 	<!--navigation bar-->
+
+	
+	<nav  class="navbar navbar-expand-lg navbar-light bg-light py-3" id="navigation">
+  <div class="container">
+<p><b><h5 style="border: solid black; padding: 5px; border-radius: 5px ;"><span style="color: coral;">Bikers</span> portal</h5></b></p>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+	<li>
+	 <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+	</li>
+  
+	</ul>
+        
+	<input type=button onClick="parent.location='customerlogin.php'"value='login'>
+	<input type=button onClick="parent.location='signup.php'"value='signup'>
+	<a href="profile.php">
+	<i class="fa fa-user" aria-hidden="true"></i>
+</a>
+<a href="addtocart.php"> 
+	<i class="fa fa-shopping-cart" aria-hidden="true"> </i>
+</a>
+    </div>
+  </div>
+</nav>
+
+<!--first image-->
+
+<section id="home">
+	<div class="container">
+		<h9>The only online bike shop in bohol</h9>
+		<h1><span><b>BIKERS</b></span> <span style="color: white">PORTAL</span></h1>
+		<h9>Provides you a comfortable online bike shopping <h9><br><br><br>
+		<a href=""><button>Shopnow</button></a>
+	</div>
+</section>
+
+<section class="container" style="background-color: black">
+
+<h1 class="container">PRODUCT</h1>
+
+
+	<div class="row-3 mx-auto container-fluid"></div>
+	<div class="product"  id="prodt" >
+		<img src="redbike.jpg" alt="" id="bike">
+		<span class="bike">bike</span>
+</div> 
+<div class="row-3 mx-auto container-fluid"></div>
+	<div class="product"  id="prodt" >
+		<img src="redbike.jpg" alt="" id="tires">
+		<span class="bike">tires</span>
+</div> 
+<div class="row mx-auto container-fluid"></div>
+	<div class="product" >
+		<img src="redbike.jpg" alt="" id="brake">
+		<span class="bike">brake</span>
+</div> 
+<div class="row mx-auto container-fluid"></div>
+	<div class="product"  id="prodt" >
+		<img src="redbike.jpg" alt="">
+		<span class="bike">bike</span>
+</div> 
+<div class="row mx-auto container-fluid"></div>
+	<div class="product"  id="prodt" >
+		<img src="redbike.jpg" alt="">
+		<span class="bike">bike</span>
+</div> 
+<div class="row mx-auto container-fluid"></div>
+	<div class="product"  id="prodt" >
+		<img src="redbike.jpg" alt="">
+		<span class="bike">bike</span>
+</div> 
+	
+</section>
+
+<footer>
+	<div>
+		<li >
+			<a class="nav-link" href="#"><b>About</b></a>
+		</li>
+		  <li >
+			<a class="nav-link" href="#"><b>Contact us</b></a>
+		</li>
+	</div>
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+</html>
